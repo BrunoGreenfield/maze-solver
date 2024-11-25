addccebf525cb4fbe470623702c55bd62318775a
Bruno Greenfield <brunogreenfielduk@gmail.com>
2024-11-25 19:56:54 +0000

    Player class created

--- main.py
+++ main.py
@@ -7,6 +7,31 @@ args = parser.parse_args()
 mazeName = args.mazename
 
 
+class Player:
+    def __init__(self):
+        self.squareCosts = []
+
+    def currentPos(self):
+        playerIndex = ''
+        for row in maze:
+            if 'A' not in row:
+                continue
+            playerIndex = str(maze.index(row)) + str(row.index('A'))
+
+        return playerIndex
+    
+    def checkGoalState(self):
+        if self.currentPos() == goalPos:
+            return True
+        return False
+    
+    # Calculates the estimated closeness of any given square, ignoring hedges (obviously)
+    def heuristic(self, givenSquare):
+        x1, y1 = map(int, givenSquare)
+        x2, y2 = map(int, goalPos)
+        return abs(x2-x1) + abs(y2-y1)
+
+
 def displayMaze():
     print()
     for row in maze:
@@ -22,31 +47,11 @@ def displayMaze():
                     print("\033[34mZ\033[0m", end=' ') # 'Z'
         print() # Create a new line after every row
 
-def currentPos():
-    playerIndex = ''
-    for row in maze:
-        if 'A' not in row:
-            continue
-        playerIndex = str(maze.index(row)) + str(row.index('A'))
-
-    return playerIndex
-
 def getGoalPos():
     for row in maze:
         if 'Z' not in row:
             continue
         return str(maze.index(row)) + str(row.index('Z'))
-    
-def checkGoalState():
-    if currentPos() == goalPos:
-        return True
-    return False
-
-# Calculates the estimated closeness of any given square, ignoring hedges (obviously)
-def heuristic(givenSquare):
-    x1, y1 = map(int, givenSquare)
-    x2, y2 = map(int, goalPos)
-    return abs(x2-x1) + abs(y2-y1)
 
 
 # Opening the maze creating a lists of lists to represent
@@ -54,18 +59,20 @@ with open(mazeName, "r") as mazeFile:
     maze = [list(row.strip()) for row in mazeFile]
 goalPos = getGoalPos() # Must be run instantly as the maze will change as the game is played
 
+player = Player()
+
 displayMaze()
 print('Goal Position:', goalPos)
-print('Current Position:', currentPos())
-print('Heuristic:', heuristic(currentPos()))
+print('Current Position:', player.currentPos())
+print('Heuristic:', player.heuristic(player.currentPos()))
 maze[3][2] = 'A'
 maze[5][0] = '0'
 displayMaze()
-print('Current Position:', currentPos())
-print('Heuristic:', heuristic(currentPos()))
+print('Current Position:', player.currentPos())
+print('Heuristic:', player.heuristic(player.currentPos()))
 maze[3][2] = '0'
 maze[0][5] = 'A'
 displayMaze()
-print('Current Position:', currentPos())
-print('Heuristic:', heuristic(currentPos()))
-print('Goal State:', checkGoalState())
+print('Current Position:', player.currentPos())
+print('Heuristic:', player.heuristic(player.currentPos()))
+print('Goal State:', player.checkGoalState())
